48a9eba475b30bf2fd226276d44470c42c63bd74
Angela Nguyen <angelanguyen@Angelas-MacBook-Pro.local>
2024-01-30 21:26:02 -0500

    Initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,6 +4,9 @@ import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
+  console.log('about to call sign in');
+  signIn();
+  console.log('called sign in');
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
@@ -11,6 +14,22 @@ export default function TabOneScreen() {
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
+}
+
+// const makeCalltoTestTwilio = () => {
+
+// }
+const {createClient} = require('@supabase/supabase-js');
+const supabase = createClient('https://zdjcnvnmwsttcoeabufp.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkamNudm5td3N0dGNvZWFidWZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDYzOTUzNjUsImV4cCI6MjAyMTk3MTM2NX0.du7DcbcOP8kIkE6Uu_qz6Fplijfjz6y_yrjDCAuL0go');
+const signIn =async () => {
+  let { error } = await supabase.auth.signInWithOtp(
+    { phone: '+14046452587'}
+  );
+
+  if(error){
+    console.log(error);
+    return;
+  }
 }
 
 const styles = StyleSheet.create({
